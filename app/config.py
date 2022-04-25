b287b8a2430bbc21a6ea2c0286abe4fc3d48da28
sumanaashok <sumanaashok4392@gmail.com>
2022-04-25 10:45:54 -0400

    Added test to check csv file is uploaded and processed

--- app/config.py
+++ app/config.py
@@ -27,4 +27,5 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SESSION_COOKIE_SECURE = False
     DEBUG = True
+    WTF_CSRF_ENABLED = False
 
--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,11 +1,15 @@
 """This test the homepage"""
 
+from flask import url_for
+
+
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
 
 def test_auth_pages(client):
     """This makes the index page"""
@@ -15,3 +19,24 @@ def test_auth_pages(client):
     assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
+
+
+def test_register_and_login(client):
+    with client:
+        register_response = client.post("/register", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test",
+            "confirm": "test123!test"
+        },
+                                        follow_redirects=True)
+
+        assert register_response.status_code == 200
+        assert register_response.request.path == url_for('auth.login')
+
+        login_response = client.post("/login", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test"
+        },
+                                     follow_redirects=True)
+        assert login_response.request.path == url_for('auth.dashboard')
+        assert login_response.status_code == 200
